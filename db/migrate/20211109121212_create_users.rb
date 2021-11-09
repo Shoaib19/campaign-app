b220814274d65327458169d313d62cf95b7ed635
shoaib019 <shoaib019@gmail.com>
2021-11-09 17:35:07 +0500

    first commit

---
+++ db/migrate/20211109121212_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :user_type
+      t.string :status
+      t.string :profession
+      t.string :service
+
+      t.timestamps
+    end
+  end
+end
